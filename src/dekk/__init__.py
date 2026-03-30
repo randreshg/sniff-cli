9023312bda05067b7cdf0f04ccc6540717dd93bb
randreshg <randres2011@gmail.com>
2026-03-30 21:27:07 +0000

    Release dekk 1.2.0

--- src/dekk/__init__.py
+++ src/dekk/__init__.py
@@ -17,7 +17,7 @@ from typing import Any
 try:
     __version__ = version("dekk")
 except PackageNotFoundError:
-    __version__ = "1.1.0"
+    __version__ = "1.2.0"
 
 # ---------------------------------------------------------------------------
 # Lazy import registry: module_path -> list of exported names
